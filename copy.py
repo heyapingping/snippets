557bc257a02d74d1af3e2a0a965a8636a9577795
heyapingping <82163982+heyapingping@users.noreply.github.com>
2021-06-13 11:52:45 +0530

    Update copy.py

--- copy.py
+++ copy.py
@@ -5,11 +5,11 @@ from pyrogram.errors import FloodWait
 import asyncio
 
 
-FROM = -10012345678
-TO = -10012345678
-PYRO_SESSION = ""
-API_ID = 1234
-API_HASH = "abcd1234edfghijkl"
+FROM = -1482836524
+TO = -1428358122
+PYRO_SESSION = "BQAVqzFiPAyqnwgkoZsKB9yhIrpBw1kiik3nCVBvVh2gbN5aXhfKJLft5pIeM0xLgyxpZHeSna3WV1_y6shLES8GkYEs45F1dhB6ntu6AKi_GwplLPi0WN5A-vfFOQ3nSvGoyMCHRq4vJsmHAQvlM8-2z5Px1E_6QHlijJ_0kHRWpmXlub9ASQjbBknXvib8gCaQFK-t0ZoovEbsur2XhEtlxpKXGch8LTEJsGG70eGq01xs6bAUBoUi9IyVs9kPinwZi6P11D8cKQRMy3QntpTTci6ZyI33iayBn4RDW_593s5qt4ORzinwtbEBKJk6YcCzCBbc4dSn929AleAKm0qONdr9jAA"
+API_ID = 3994297
+API_HASH = "8273eb7b5b2dcf60a6ecb9eba727c473"
 
 user = Client(
     PYRO_SESSION,
